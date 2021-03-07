e1920de1a9442dc9297f1601e87be66825725bc3
Murat <mrtgms3561@gmail.com>
2021-03-07 13:57:13 +0200

    React Component Created

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:'lightblue', display:'grid', height:'100vh', placeContent:'center'}}>
+      <h1 style={{backgroundColor:"yellow", color:"blue"}}>Hello World!</h1>
+      <h2>And Hello React!!!</h2>
     </div>
   );
 }
